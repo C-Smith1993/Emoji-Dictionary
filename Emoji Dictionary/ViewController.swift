346815eac2a4f9b6d7e4071f958723494df03232
C-Smith1993 <chrissmithreds93@hotmail.com>
2017-02-20 22:34:34 +0000

    A few minor changes were made.

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -38,8 +38,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let emojiIcon = emojiIcons[indexPath.row]
-        performSegue(withIdentifier: "moveSegue", sender: emojiIcon)
+        let emoji = emojiIcons[indexPath.row]
+        performSegue(withIdentifier: "moveSegue", sender: emoji)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
